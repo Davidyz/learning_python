028dfa263deab6d44aaff96393d47aca74dc5d90
Davidyz <hzjlyz@gmail.com>
2018-03-30 18:01:31 +0800

    Function of newton's method is improved adn bug in differentiate() is solved.

--- module/maths.py
+++ module/maths.py
@@ -113,17 +113,16 @@ def differentiate(f, x):
     """
     Find the gradient of the curve f at the point (x, f(x))
     """
-    step = 1 / 0.00001
+    step = 0.0000000001
     return (f(x + step) - f(x)) / step
 
-def newton_method(f, accu = 3):
+def newton_method(f, x = 1, accu = 3):
     """
     Find one of the roots of equation f using Newton's method.
     f is the function of the equation.
     """
-    x = 10
     y = 1
-    while y > 1 / 10 ** (accu):
+    while y > 1 / 10 ** (accu + 5):
         y = f(x)
         m = differentiate(f, x)
         x = x - y / m
