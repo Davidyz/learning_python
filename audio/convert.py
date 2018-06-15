d6c75898a5a235aac7359167f51fa59047f67834
Davidyz <hzjlyz@gmail.com>
2018-06-15 22:19:43 +0800

    Convert lossless music files (excluding dsd) into flac format in batch.

--- audio/convert.py
+++ audio/convert.py
@@ -1,9 +1,80 @@
+#!/usr/bin/python3
+#coding=utf-8
 import os
-path = '/mnt/c/Users/30813/Music'
-types = ['wav', 'ape']
-command = '''track2track -t flac "{path}/{song}" -o "{output}"'''
-for i in os.listdir(path):
-    if i.split('.')[-1] in types:
-        os.system(command.format(path=path,
-                                 song=i,
-                                 output="/mnt/c/Users/30813/Desktop/" + i))
+command = '''ffmpeg -i "{origin}" "{output}"'''
+rootdir = '/mnt/c/Users/30813/Music/'
+to_be_converted = ['wav', 'ape']
+music_files = ['wav', 'ape', 'flac', 'mp3', 'm4a', 'dsf', 'dff']
+singers_and_albums = {}
+all_songs = []
+
+def is_song(item):
+    if item.split('.')[-1] in music_files:
+        return True
+    return False
+
+def set_singers():
+    '''
+    Generate a dict where the keys are the names of the artists.
+    '''
+    for i in os.listdir(rootdir):
+        if os.path.isdir(rootdir + i):
+            singers_and_albums[i] = []
+
+def set_albums():
+    '''
+    Append the albums of each artist into the dict.
+    '''
+    for i in singers_and_albums:    # iter through each singer
+        for j in os.listdir(rootdir + i):   # iter through each album of the singer
+            if os.path.isdir(rootdir + i + '/' + j):
+                singers_and_albums[i].append(j)
+
+def title(song):
+    '''
+    Return the title of the song based on the file name.
+    '''
+    song = song.split('.')[:-1]
+    name = ''
+    for i in song:
+        name += i
+    return name
+
+def gen_song_list():
+    '''
+    Generate a list of the abs directory of the songs.
+    '''
+    # First step (no specified singer)
+    for i in os.listdir(rootdir):
+        if is_song(i):
+            all_songs.append(rootdir + i)
+
+    # Second step (specified singer, non-specified album)
+    for i in singers_and_albums:    # iter through each singer
+        for j in os.listdir(rootdir + i):   # iter through each item
+            if is_song(j):
+                all_songs.append(rootdir + i + '/' + j)
+
+    # Third step (specified singer and specified album)
+    for singer in singers_and_albums:    # iter through each singer
+        for album in singers_and_albums[singer]:    # iter through each album of the singer
+            for item in os.listdir(rootdir + singer + '/' + album):    # iter through each item in the album
+                if is_song(item):
+                    all_songs.append(rootdir + singer + '/' + album + '/' + item)
+
+def convert_into_flac(list_of_songs):
+    for i in range(len(list_of_songs)):
+        if list_of_songs[i].split('.')[-1] in to_be_converted:
+            name = title(list_of_songs[i])
+            print('Converting {}'.format(name))
+            os.system(command.format(origin=list_of_songs[i], output=name + '.flac'))
+            list_of_songs.pop(i)
+            #os.system('rm {}'.format(list_of_songs.pop(i)))
+            list_of_songs.insert(i, name + '.flac')
+
+if __name__ == '__main__':
+    set_singers()
+    set_albums()
+    gen_song_list()
+    convert_into_flac(all_songs)
+    print(list_of_songs)
