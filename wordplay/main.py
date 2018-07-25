9cdef1010a28e6fc950ea112ef2880f91990e5f1
Davidyz <hzjlyz@gmail.com>
2018-07-25 12:17:55 +0100

    New wordlist uploaded.

--- wordplay/main.py
+++ wordplay/main.py
@@ -1,46 +1,48 @@
+#!/usr/bin/env python3
 import itertools, sys
+sys.path.append('/home/david/git/learning_python/module/')
+import sort
 
-fin = open('wordplay/wordlist.txt', 'r')
+fin = open('wordplay/google-10000-english.txt', 'r')
 wordlist = fin.readlines()
 fin.close()
-wordlist = list(i[:-1].lower() for i in wordlist)
-letters = str(sys.argv[-1]).lower()
-permutation = []
+wordlist = sort.quicksort(list(i[:-1].lower() for i in wordlist))
+print(wordlist)
+
+if len(sys.argv) == 2:
+    letters = str(sys.argv[-1]).lower()
+else:
+    print('Invalid argument!')
+    exit()
+
 results = []
 
-def join(array):
-    string = ''
-    for i in array:
-        string += str(i)
-    return string
-
 def binary_search(word, array, start=0, end=None):
+    
     if end == None:
         end = len(array) - 1
     if start == end:
         if word == array[start]:
             return start
         else:
-            return None
-    if start < end:
+            return -1
+    
+    elif start < end:
         middle = (start + end) // 2
+        
         if word == array[middle]:
             return middle
+        
         elif word > array[middle]:
             return binary_search(word, array, middle + 1, end)
+        
         elif word < array[middle]:
             return binary_search(word, array, start, middle - 1)
 
-for i in range(1, len(letters) + 1):
-    permutation += list(itertools.permutations(letters, i))
-
-for i in range(len(permutation)):
-    permutation[i] = join(permutation[i])
-
-for i in permutation:
-    if binary_search(i, wordlist) != None:
-        if not i in results:
-            results.append(i)
-
-for i in results:
-    print(i)
+for i in range(len(letters), 0, -1):
+    for j in itertools.permutations(letters, i):
+        j = ''.join(j)
+        if binary_search(j, wordlist) != -1:
+            if not j in results: #j binary_search(j, results) == -1:
+                print(j)
+                results.append(j)
