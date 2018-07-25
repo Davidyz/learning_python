e5935b690149adac613e43eb247d2759fc010133
Davidyz <hzjlyz@gmail.com>
2018-07-25 12:17:23 +0100

    Class notes uploaded.

--- Oxbridge/practice/circle.py
+++ Oxbridge/practice/circle.py
@@ -2,11 +2,16 @@ from math import pi
 class Circle():
     '''
     A class for circle.
+    Author: David
+    Date: 25, 7, 2018.
     '''
-    def __init__(self, r):
-        self.radius = r
+    def __init__(self, radius, xcor = 0, ycor = 0):
+        self.r = radius
+        self.x0 = xcor
+        self.y0 = ycor
 
     def area(self):
-        return pi * (self.radius ** 2)
+        return pi * (self.r ** 2)
 
-
+    def circumference(self):
+        return 2 * pi * self.r
---
+++ Oxbridge/practice/p0725.py
@@ -0,0 +1,34 @@
+class Fraction():
+    '''
+    Author: Davidyz
+    Date: 25, 7, 2018
+    你们啊，Naive!
+    '''
+    
+    def __init__(self, numerator, dominator):
+        self.__numerator = numerator
+        self.__dominator = dominator
+
+    def get_numerator(self):
+        return self.__numerator
+
+    def get_dominator(self):
+        return self.__dominator
+
+    def set_numerator(self, value):
+        self.numerator = value
+
+    def set_dominator(self, value):
+        self.dominator = value
+
+if __name__ == '__main__':
+    F1 = Fraction(2,5)
+    
+    try:
+        print(F1.__numerator)
+        print(F1.__dominator)
+    except AttributeError:
+        print('Private attributes!')
+    
+    print(F1.get_numerator())
+    print(F1.get_dominator())
