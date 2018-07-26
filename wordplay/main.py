7896566586e5c90a5d31fb623c929816d65e6fca
Davidyz <hzjlyz@gmail.com>
2018-07-26 16:10:11 +0100

    The google wordlist is sorted and main.py is improved.

--- wordplay/main.py
+++ wordplay/main.py
@@ -3,11 +3,10 @@ import itertools, sys
 sys.path.append('/home/david/git/learning_python/module/')
 import sort
 
-fin = open('wordplay/google-10000-english.txt', 'r')
+fin = open('wordplay/new_google-10000-english.txt', 'r')
 wordlist = fin.readlines()
 fin.close()
-wordlist = sort.quicksort(list(i[:-1].lower() for i in wordlist))
-print(wordlist)
+wordlist = list(i[:-1].lower() for i in wordlist)
 
 if len(sys.argv) == 2:
     letters = str(sys.argv[-1]).lower()
@@ -25,7 +24,7 @@ def binary_search(word, array, start=0, end=None):
         if word == array[start]:
             return start
         else:
-            return -1
+            return None
     
     elif start < end:
         middle = (start + end) // 2
@@ -39,10 +38,10 @@ def binary_search(word, array, start=0, end=None):
         elif word < array[middle]:
             return binary_search(word, array, start, middle - 1)
 
-for i in range(len(letters), 0, -1):
+for i in range(len(letters)):
     for j in itertools.permutations(letters, i):
         j = ''.join(j)
-        if binary_search(j, wordlist) != -1:
+        if binary_search(j, wordlist) != None:
             if not j in results: #j binary_search(j, results) == -1:
                 print(j)
                 results.append(j)
---
+++ wordplay/sort_wordlist.py
@@ -0,0 +1,18 @@
+import sys, os
+sys.path.append('/home/david/git/learning_python/module/')
+import sort
+
+target_dir = '/home/david/git/learning_python/wordplay/'
+fin = open(sys.argv[1], 'r')
+wordlist = sort.quicksort(list(i[:-1] for i in fin.readlines()))
+fin.close()
+
+new_file_name = 'new_{}'.format(sys.argv[1].split('/')[-1])
+
+if not new_file_name in os.listdir(target_dir):
+    os.system('touch {}'.format(target_dir + new_file_name))
+
+target_file = open(target_dir + new_file_name, 'w')
+for i in wordlist:
+    target_file.write(i + '\n')
+target_file.close()
