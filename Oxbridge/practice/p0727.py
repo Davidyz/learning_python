236794262046e9bc86f26b8aaba9f96214d3df7e
Davidyz <hzjlyz@gmail.com>
2018-07-27 12:18:39 +0100

    Classwork uploaded.

--- Oxbridge/practice/p0727.py
+++ Oxbridge/practice/p0727.py
@@ -1,3 +1,4 @@
+import math, itertools, sys
 class A():
     def __init__(self):
         self.name = 'A'
@@ -18,9 +19,10 @@ class Polygon():
     Author: Davidyz;
     Date: 27, 7, 2018
     '''
-    def __init__(self, sides):
-        self.n = len(sides)
-        self.sides = sides
+    def __init__(self, num_of_sides):
+        self.n = num_of_sides
+
+        self.sides = list(0 for i in range(num_of_sides))
     
     def input_sides(self, array):
         if len(array) == self.n:
@@ -29,18 +31,31 @@ class Polygon():
             raise IndexError('Unmatched number of sides!')
 
     def display_side(self):
-        for i in self.sides:
-            print(i)
+        for i in range(len(self.sides)):
+            print('Side{}: {}'.format(i + 1, self.sides[i]))
+
+def valid_triangle(array):
+    array.sort()
+    return not sum(array[:2]) <= array[2]
+
+class Triangle(Polygon):
+    '''
+    Author: Davidyz;
+    Date: 27, 7, 2018
+    '''
+    def __init__(self, length):
+        Polygon.__init__(self, 3)
+        if valid_triangle(length):
+            self.sides = length
+        else:
+            raise ValueError('Invalid lengths for a triangle!')
+
+    def area(self):
+        s = sum(self.sides) / 2
+        return math.sqrt(s * (s - self.sides[0]) * (s - self.sides[1]) * (s - self.sides[2]))
 
 if __name__ == '__main__':
-
-    b = B()
-
-    try:
-        b.printA()
-        print(b.name)
-    except Exception as er:
-        print(er)
-
-    b.printB()
-    print(b.name2)
+    length = [float(i) for i in sys.argv[1:]]
+    t1 = Triangle(length)
+    t1.display_side()
+    print(t1.area())
