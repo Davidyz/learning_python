3eeed3fb5cd83400be1984d73611032f67c3d5db
Davidyz <hzjlyz@gmail.com>
2018-08-02 23:09:39 +0100

    Homework completed.

--- Oxbridge/practice/assessment3.py
+++ Oxbridge/practice/assessment3.py
@@ -6,12 +6,12 @@ from scipy.optimize import curve_fit
 import pylab
 
 print("\n\nThe graphs will be saved in the working directory instead of simply showing!!!\n\n")
-# Q1
+############################# Q1 ###########################
 table = pd.read_csv('skyserver.csv')
 rows = table.shape[0]
 columns_no = table.shape[1]
 
-# Q2
+############################# Q2 ###########################
 def object_type():
     
     objects = {'GALAXY':0,
@@ -27,38 +27,30 @@ def object_type():
 
 object_type()
 
-# Q3
+############################# Q3 ############################
 table_galaxy = table[table['class'] == 'GALAXY']
 
-# Q4
-def plot_galaxy():
-    temp = table_galaxy[table_galaxy['ra'] <= 180]
-    temp = temp[temp['ra'] >= 150]
-    temp = temp[temp['dec'] >= 10]
-    temp = temp[temp['dec'] <= 40]
-    return temp
-
-ploted_galaxy = plot_galaxy()
-
-plt.scatter(ploted_galaxy['ra'],
-            ploted_galaxy['dec'],
-            c=ploted_galaxy['redshift'],
+############################# Q4 ############################
+plt.scatter(table_galaxy['ra'],
+            table_galaxy['dec'],
+            c=table_galaxy['redshift'],
             s=1)
 
+plt.xlim(150, 180)
+plt.ylim(10, 40)
 plt.colorbar()
 plt.title('The Positions of Galaxies')
 plt.xlabel('RA(degree)')
 plt.ylabel('DEC(degree)')
 plt.savefig('Q4.png')
 plt.hold(False)
-del ploted_galaxy
 
-# Q5
+############################# Q5 ##############################
 table_galaxy['color'] = table_galaxy['u'] - table_galaxy['r']
 
 plt.hist(table_galaxy['color'],
-         bins = 1000,
-         range=(0, 4))
+         bins = 1000)
+plt.xlim(0, 4)
 
 def names():
     plt.title('The Distribution of the Color of Galaxies, ie. u - r')
@@ -70,19 +62,18 @@ plt.savefig('Q5.png')
 plt.hold(True)
 plt.show()
 
-# Q6
-data = table_galaxy['color']
-y, x, _ = plt.hist(data, bins = 1000, alpha = 1, label = 'counts')
+############################# Q6 ###############################
+y, x, _ = plt.hist(table_galaxy['u'] - table_galaxy['r'], bins = 1000, alpha = 1, label = 'counts')
 x = (x[1:] + x[:-1]) / 2
 mean1, mean2 = 0, 0
 
-def gauss(x,mu,sigma,A):
-    return A * pylab.exp(-(x-mu)**2/2/sigma**2)
+def gauss(x, mu, sigma, A):
+    return A * pylab.exp(-(x - mu) ** 2 / 2 / sigma ** 2)
 
-def bimodal(x,mu1,sigma1,A1,mu2,sigma2,A2):
+def bimodal(x, mu1, sigma1, A1, mu2, sigma2, A2):
     global mean1, mean2
     mean1, mean2 = mu1, mu2
-    return gauss(x,mu1,sigma1,A1)+gauss(x,mu2,sigma2,A2)
+    return gauss(x, mu1, sigma1, A1) + gauss(x, mu2, sigma2, A2)
 
 expected = (1, .2, 250, 2, .2, 125)
 params, cov = curve_fit(bimodal, x, y, expected)
@@ -99,5 +90,5 @@ names()
 plt.savefig('Q6.png')
 plt.show()
 
-# Q7
+############################## Q7 ################################
 print('\nThe values of colors are {} and {}.'.format(mean1, mean2))
