e114d755cd1ef0ad4b9471cd3d9e03c81ef779c5
Davidyz <hzjlyz@gmail.com>
2019-03-19 06:06:16 +0800

    Rewriting integration.py using dict to improve efficiency.

--- module/integration.py
+++ module/integration.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
 from math import *
+import maths
 """
 A set of functions for numerical integration, giving the results as float.
 You should always specify the number of intervals though it is unnecessary, because a fixed number of strips won't work efficiently. Eg, when the range is big, the number of strips should be increased as well, and vice versa (for less time and resource consumption).
 """
+
+def mapping(f, start_point, end_point, num):
+    """
+    Map the function into a dictionary.
+    """
+    res = {start_point:f(start_point)}
+    interval = (end_point - start_point) / float(num)
+
+    for i in range(num + 1):
+        res[start_point + interval * i] = f(start_point + i * interval)
+    
+    return res
 
 def xcor(n, start_point, r, num):
     """
@@ -18,7 +31,6 @@ def area(f, n, start_point, r, num):
     For mid_point rule.
     """
     return f(xcor(n, start_point, r, num)) * (r / num)
-
 
 def mid_point(f, start_point, end_point, num=10000):
     """
@@ -47,15 +59,17 @@ def trapezium(f, start_point, end_point, num=10000):
     """
     Using trapezoidal rule of integration.
     """
-    r = end_point - start_point
     total_area = 0
-    width = r / num   # the width of trapezoidal.
-
-    ycor = y(f, num, start_point, width)       
+    interval = (end_point - start_point) / float(num)
     
-    for i in range(len(ycor) - 1):
-        a = (ycor[i] + ycor[i + 1]) * width / 2
-        total_area += a
+    critical_values = mapping(f, start_point, end_point, num)
+    index = tuple(critical_values.keys())
+
+    for i in range(len(index)):
+        total_area += 2 * critical_values[index[i]]
+
+    total_area -= critical_values[index[i]] + critical_values[index[i]]
+    total_area = total_area * interval / 2
 
     return total_area
 
@@ -65,3 +79,5 @@ def simpson(f, start_point, end_point, num=10000):
     """
     if num % 2 == 0:
         return (2 * mid_point(f, start_point, end_point, num / 2) + trapezium(f, start_point, end_point, num / 2)) / 3
+    else:
+        raise ValueError("num must be an even number!")
