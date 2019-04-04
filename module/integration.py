6a2d81f7bcf051f543d021db9ec6166dbf411d5e
Davidyz <hzjlyz@gmail.com>
2019-04-04 19:13:08 +0800

    Re-written with mapping() for efficiency.

--- module/integration.py
+++ module/integration.py
@@ -5,63 +5,78 @@ A set of functions for numerical integration, giving the results as float.
 You should always specify the number of intervals though it is unnecessary, because a fixed number of strips won't work efficiently. Eg, when the range is big, the number of strips should be increased as well, and vice versa (for less time and resource consumption).
 """
 
-def xcor(n, start_point, r, num):
+def mapping(f, start_point, end_point, num):
     """
-    Return the x cordinate of the nth rectangle.
-    For mid_point rule.
+    Map the function into a dictionary.
     """
-    return start_point + 0.5 * (r / num) + (n - 1) * (r / num)
+    res = {start_point:f(start_point)}
+    interval = (end_point - start_point) / float(num)
 
-def area(f, n, start_point, r, num):
-    """
-    Return the area of the nth rectangle.
-    For mid_point rule.
-    """
-    return f(xcor(n, start_point, r, num)) * (r / num)
+    for i in range(int(num) + 1):
+        res[start_point + interval * i] = f(start_point + i * interval)
+    
+    return res
 
-
-def mid_point(f, start_point, end_point, num=10000):
+def mid_point(f, start_point, end_point, num=10000, table = None):
     """
     Using the mid-point rule of integration.
+    Re-written with mapping().
     """
-    r = end_point - start_point
     total_area = 0
+    interval = (end_point - start_point) / float(num)
 
-    for i in range(1, int(num) + 1):
-        total_area += area(f, i, start_point, r, num)
-    
+    if table == None:
+        critical_values = mapping(f, start_point + interval / 2, end_point - interval / 2, num - 1)
+    else:
+        critical_values = table
+    index = tuple(critical_values.keys())
+
+    for i in range(len(index)):
+        total_area += interval * critical_values[index[i]]
+
     return total_area
 
-def y(f, n, start_point, width):
-    """
-    Return the list of y-cordinates.
-    For trapezium rule.
-    """
-    result = []
-    for i in range(int(n) + 1):
-        result.append(f(start_point + i * width))
-      
-    return result
-    
-def trapezium(f, start_point, end_point, num=10000):
+def trapezium(f, start_point, end_point, num=10000, table = None):
     """
     Using trapezoidal rule of integration.
+    Re-written with mapping().
     """
-    r = end_point - start_point
     total_area = 0
-    width = r / num   # the width of trapezoidal.
-
-    ycor = y(f, num, start_point, width)       
+    interval = (end_point - start_point) / float(num)
     
-    for i in range(len(ycor) - 1):
-        a = (ycor[i] + ycor[i + 1]) * width / 2
-        total_area += a
+    if table == None:
+        critical_values = mapping(f, start_point, end_point, num)
+    else:
+        critical_values = table
+    index = tuple(critical_values.keys())
+
+    for i in range(len(index)):
+        total_area += 2 * critical_values[index[i]]
+
+    total_area -= (critical_values[index[0]] + critical_values[index[-1]])
+    total_area = total_area * interval / 2
 
     return total_area
 
 def simpson(f, start_point, end_point, num=10000):
     """
     Using simpson's rule to integrate.
+    Re-written with mapping().
     """
     if num % 2 == 0:
-        return (2 * mid_point(f, start_point, end_point, num / 2) + trapezium(f, start_point, end_point, num / 2)) / 3
+        num = num // 2
+        table = mapping(f, start_point, end_point, num)
+        return (2 * mid_point(f, start_point, end_point, num, table) + trapezium(f, start_point, end_point, num, table)) / 3
+    else:
+        raise ValueError("num must be an even number!")
+
+if __name__ == "__main__":
+    def f(x):
+        return sqrt(x ** 2 - 1)
+    
+    start_point = 1
+    end_point = 2
+    num = 100000
+    print("simpson  ", simpson(f, start_point, end_point, num))
+    print("trapezium", trapezium(f, start_point, end_point, num))
+    print("mid_point", mid_point(f, start_point, end_point, num))
