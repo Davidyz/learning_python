d899edeaece20d7a7a876f4af4aaa66c3b420851
Davidyz <hzjlyz@gmail.com>
2019-07-06 14:18:14 +0800

    Doc strings are added.

--- module/Sudoku.py
+++ module/Sudoku.py
@@ -1,46 +1,58 @@
-import sort, numpy
+import Array, numpy, copy
 index = [0, 0]
 
 def _is_complete(sudoku):
-    if len(sudoku) != 9:
-        return False
+    '''
+    Check whether a sudoku is completely filled in.
+    '''
     for i in sudoku:
-        if len(i) != 9:
+        if 0 in i:
             return False
     return True
 
-def _check(array, element={1,2,3,4,5,6,7,8,9}):
-    if element == set(sort.mergesort(array)):
-        return len(array) == len(set(array))
-    else:
-        return False
+def _check(array):
+    '''
+    Check whether the array is a valid one in a sudoku.
+    '''
+    temp = copy.deepcopy(array)
+    temp = Array.clear_item(temp, 0)
+    return len(temp) == len(set(temp))
 
 def add_index(index):
-    if index[1] < 8:
+    '''
+    Return the next index in sudoku.
+    '''
+    if index == [8,8]:
+        return None
+    elif index[1] < 8:
         return [index[0], index[1] + 1]
     elif index[1] == 8:
         return [index[0] + 1, 0]
+
+def sub_index(index):
+    '''
+    Return the last index in sudoku.
+    '''
+    if index[1] == 0:
+        return [index[0] - 1, 8]
+    elif index[1] > 0:
+        return [index[0], index[1] - 1]
 
 def pprint(sudoku):
     '''
     Print a Sudoku puzzle that is defined as a list with 9 items such that every item represent one of the 9 rows.
     '''
-    index = [0, 0]
-    while index != [9, 0]:
-        if index[1] == 0 and index[0] != 0:
-            print('\n')
-        if index[1] % 3 == 0 and index[1] > 0:
-            print('    ', end='')
-        if index[0] % 3 == 0 and index[1] == 0:
-            print('\n')
-        if sudoku[index[0]][index[1]] != 0:
-            print(sudoku[index[0]][index[1]], end=' ')
-        else:
-            print('□', end=' ')
-        index = add_index(index)
+    print("+-------+-------+-------+")
+    for i in range(9):
+        print("| {} {} {} | {} {} {} | {} {} {} |".format(*sudoku[i]).replace("0"," "))
+        if (i+1) % 3 == 0:
+            print("+-------+-------+-------+")
     print('\n')
 
 def gen_blocks(sudoku):
+    '''
+    Return blocks in a sudoku.
+    '''
     blocks = list([] for i in range(9))
     for i in range(9):
         row = i // 3
@@ -50,6 +62,9 @@ def gen_blocks(sudoku):
     return blocks
 
 def check_sudoku(sudoku):
+    '''
+    Check the validity of a sudoku.
+    '''
     # check rows
     for i in sudoku:
         if not _check(i):
@@ -73,11 +88,27 @@ def check_sudoku(sudoku):
     return True
             
 def _enter():
+    '''
+    Enter a sudoku from console.
+    '''
     sudoku = []
     for i in range(9):
         char = input("Enter the {}th row (all-together, eg: 123456789): ".format(str(i + 1)))
+        
         sudoku.append(list(int(j) for j in char))
     return sudoku
+
+def first_empty(sudoku):
+    '''
+    Return the index of the first empty cell in a sudoku.
+    '''
+    index = [0, 0]
+    while sudoku[index[0]][index[1]] != 0:
+        if index == [8, 8]:
+            return None
+        index = add_index(index)
+
+    return index
 
 if __name__ == '__main__':
     _sample = [[9,8,0,2,0,0,0,1,0],
@@ -103,6 +134,6 @@ if __name__ == '__main__':
     _wrong = _correct
     _wrong[0][0] = 2
     
-    pprint(_wrong)
+    pprint(_sample)
     print('=' * 50)
-    print(check_sudoku(_wrong))
+    print(first_non_empty(_wrong))
