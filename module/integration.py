83157eda607fc2b33a3d6da783878f88351adeb1
Davidyz <hzjlyz@gmail.com>
2019-09-01 16:11:18 +0800

    Monte Carlo method is added.

--- module/integration.py
+++ module/integration.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from math import *
+import maths
 """
 A set of functions for numerical integration, giving the results as float.
 You should always specify the number of intervals though it is not necessary, because a fixed number of strips won't work efficiently. Eg, when the range is big, the number of strips should be increased as well, and vice versa (for less time and resource consumption).
@@ -78,13 +79,54 @@ def simpson(f, start_point, end_point, num=10000):
     else:
         raise ValueError("num must be an even number!")
 
+def LocalStationary(f, a, b, accu = 5):
+    import random
+    a, b = min(a, b), max(a, b)
+    data = {}
+    for i in range(int((b - a) // 10 ** (-accu))):
+        x = random.uniform(a, b)
+        y = f(x)
+        data[x] = y
+
+    maximum = (0, -inf)
+    minimum = (0, inf)
+
+    for i in data:
+        if data[i] > maximum[1]:
+            maximum = (i, data[i])
+        if data[i] < minimum[1]:
+            minimum = (i, data[i])
+
+    return (round(minimum[1], accu), round(maximum[1], accu))
+
+def MonteCarlo(f, a, b, accu=5):
+    import random
+    a, b = min(a, b), max(a, b)
+    count_total = 0
+    count_valid = 0
+    r = LocalStationary(f, a, b, accu)
+    if r[0] * r[1] >= 0:
+        rect = abs(r[0] - r[1]) * (b - a)
+        if r[1] <= 0:
+            rect = -rect
+    else:
+        rect = abs(r[0] + r[1]) * (b - a)
+
+    for i in range(int((b - a) // 10 ** (-accu))):
+        x, y = random.uniform(a, b), random.uniform(r[0], r[1])
+        count_total += 1
+        if (y < f(x) and f(x) > 0) or (y > f(x) and f(x) <= 0):
+            count_valid += 1
+
+    return round(count_valid / count_total * rect, accu)
+    
 if __name__ == "__main__":
     def f(x):
         return e ** (- x ** 2 / 2)
     
-    start_point = -1000
-    end_point = 1000
-    num = 10000
+    start_point = -100
+    end_point = 100
+    num = 500
     print("simpson  ", simpson(f, start_point, end_point, num))
     print("trapezium", trapezium(f, start_point, end_point, num))
     print("mid_point", mid_point(f, start_point, end_point, num))
