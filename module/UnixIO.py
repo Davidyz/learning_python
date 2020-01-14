1c953cad22409608ba7966b48ec6ac63ab65c4b7
Davidyz <hzjlyz@gmail.com>
2020-01-14 17:34:47 +0800

    New module added.

---
+++ module/UnixIO.py
@@ -0,0 +1,30 @@
+import os
+def listdir(path):
+    stack = [os.path.join(path, i) for i in os.listdir(path)]
+    output = []
+    while stack:
+        candidate = stack.pop(-1)
+        if os.path.isfile(candidate):
+            output.append(candidate)
+        else:
+            for i in os.listdir(candidate):
+                stack.append(os.path.join(candidate, i))
+    return output
+
+def readfile(path, binary=False):
+    option = 'r'
+    if binary:
+        option += 'b'
+    fin = open(path, option)
+    content = fin.readlines()
+    fin.close()
+    return content
+
+def writefile(path, content):
+    fin = open(path, 'w')
+    if isinstance(content, str):
+        fin.writeline(content)
+    elif isinstance(content, list):
+        fin.writelines(content)
+    fin.close()
+
