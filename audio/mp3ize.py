d6342ca795707e3e4127303907d2daa5f7a89606
Davidyz <hzjlyz@gmail.com>
2020-01-31 21:28:39 +0800

    Convert songs to mp3 in batch for mobile devices.

---
+++ audio/mp3ize.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import music, UnixIO, sys, os, multiprocessing
+arguments = sys.argv
+
+replaced = '-r' in arguments
+if replaced:
+    arguments.remove('-r')
+
+if len(arguments) == 3:
+    for i in arguments[1:]:
+        if (not os.path.isfile(i)) and (not os.path.isdir(i)):
+            raise music.InputError('The input {} is not valid!'.format(i))
+
+    args = {'original':arguments[1],'destination':arguments[2]}
+elif len(arguments) == 2 and (os.path.isfile(arguments[1]) or os.path.isdir(arguments[1])):
+    args = {'original':arguments[1],'destination':False}
+
+songs = [music.Music(i) for i in UnixIO.listdir(args['original']) if music.is_music(i)]
+
+def execute(song, replace, to):
+    if replace:
+        song.format('mp3', True)
+        return
+    song.format('mp3', False)
+    if to:
+        UnixIO.mv(song.path().replace(song.form, 'mp3'), to)
+
+pool = multiprocessing.Pool(processes = 3)
+pool.starmap_async(execute, ([i, replaced, args['destination']] for i in songs)).get()
+pool.close()
+pool.join()
