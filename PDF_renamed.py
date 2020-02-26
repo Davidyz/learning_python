f8860e6140a9087fc3e45a2f5e8542f61e2bfa16
Davidyz <hzjlyz@gmail.com>
2020-02-26 13:25:30 +0800

    Operation reduced.

--- PDF_renamed.py
+++ PDF_renamed.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-import os, time
-from module import UnixIO
+import os, time, UnixIO
 path = '/home/davidyz/SFTP/2020 Online Edu/Further maths/Assignment/'
 while True:
     files = [i for i in UnixIO.listdir(path) if 'compressed' in i and (not 'Mechanics' in i)]
@@ -8,9 +7,12 @@ while True:
         if '\xa0' in i:
             UnixIO.mv(i, i.replace('\xa0', ' '))
             files.remove(i)
-            files.append(i.replace('\xa0', ' ')
+            files.append(i.replace('\xa0', ' '))
     
     for i in files:
-        UnixIO.cp(i, i.replace(i.split('/')[-1], "A2-C David {subject} {index}.pdf".format(subject = i.split('/')[-2][:4].upper(),
-                                                                                           index = i.split('/')[-1].split(' ')[1])))
+        new = i.replace(i.split('/')[-1], "A2-C David {subject} {index}.pdf".format(subject = i.split('/')[-2][:4].upper(),
+                                                                                    index = i.split('/')[-1].split(' ')[1]))
+        if os.path.isfile(new):
+            continue
+        UnixIO.cp(i, new)
     time.sleep(10)
