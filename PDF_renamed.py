97a31d90bf581b65259ffddc518cf832dcf510ff
Davidyz <hzjlyz@gmail.com>
2020-02-26 12:55:36 +0800

    GBK bug fixed.

--- PDF_renamed.py
+++ PDF_renamed.py
@@ -5,6 +5,12 @@ path = '/home/davidyz/SFTP/2020 Online Edu/Further maths/Assignment/'
 while True:
     files = [i for i in UnixIO.listdir(path) if 'compressed' in i and (not 'Mechanics' in i)]
     for i in files:
-        UnixIO.cp(i, "A2-C David {subject} {index}.pdf".format(subject = i.split('/')[-2][:4].upper(),
-                                                               index = i.split('/'.)split(' ')[1]))
+        if '\xa0' in i:
+            UnixIO.mv(i, i.replace('\xa0', ' '))
+            files.remove(i)
+            files.append(i.replace('\xa0', ' ')
+    
+    for i in files:
+        UnixIO.cp(i, i.replace(i.split('/')[-1], "A2-C David {subject} {index}.pdf".format(subject = i.split('/')[-2][:4].upper(),
+                                                                                           index = i.split('/')[-1].split(' ')[1])))
     time.sleep(10)
