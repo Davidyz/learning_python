679f61fec84462ac9533f7ff16decc48b662e83f
Davidyz <hzjlyz@gmail.com>
2020-02-26 12:26:38 +0800

    New file for PDF renaming.

---
+++ PDF_renamed.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import os, time
+from module import UnixIO
+path = '/home/davidyz/SFTP/2020 Online Edu/Further maths/Assignment/'
+while True:
+    files = [i for i in UnixIO.listdir(path) if 'compressed' in i and (not 'Mechanics' in i)]
+    for i in files:
+        UnixIO.cp(i, "A2-C David {subject} {index}.pdf".format(subject = i.split('/')[-2][:4].upper(),
+                                                               index = i.split(' ')[-1].split('.')[0]))
+    time.sleep(10)
