9952aa9fd94dc3b12e67b2d632b699d14495344c
Davidyz <hzjlyz@gmail.com>
2020-06-17 18:40:50 +0800

    Transcode h264 videos into h265 (hevc).

--- NAS/transcode.py
+++ NAS/transcode.py
@@ -1,5 +1,27 @@
-import UnixIO, os, time
+import UnixIO, os, time, cv2
+import sys
+args = sys.argv
 
-path = '/home/davidyz/SFTP/Videos/'
-files = [i for i in os.listdir(path) if (('mkv' in i) or ('mp4' in i)) and (not 'remastered' in i)]
+old_path = args[1]
+new_path = args[2]
 
+for i in [old_path, new_path]:
+    if i[-1] != '/':
+        i += '/'
+
+if old_path == new_path:
+    os.system('mkdir tmp')
+    for i in os.listdir(old_path):
+        UnixIO.mv(i, 'tmp')
+    old_path = 'tmp/'
+    tmp = True
+else:
+    tmp = False
+
+files = [i for i in UnixIO.listdir(old_path)]
+
+for i in files:
+    os.system('ffmpeg -i "{source}" -c:v libx265 -c:a libfdk_aac -y -q 0 -map 0 -y "{target}"'.format(source=i, target=i.replace('264', '265').replace(old_path, new_path)))
+
+if tmp:
+    os.system('rm -r tmp')
